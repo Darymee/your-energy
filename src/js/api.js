c3a0f4289afbb2333d6433e7f817b460af2eaa52
AlexDvor <alexmatvichuk@gmail.com>
2025-11-18 14:25:00 +0100

    ref

--- src/js/api.js
+++ src/js/api.js
@@ -15,7 +15,6 @@ class Api {
     const res = await axios.get('/filters', {
       params: this.#getSearchParams('Muscles'),
     });
-    console.log('🚀 ~ res:', res);
     return res.data;
   }
 }
