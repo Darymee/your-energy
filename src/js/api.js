48a89b9270c96ea18e404a2497d0ceaa0ff04bd0
AlexDvor <alexmatvichuk@gmail.com>
2025-11-18 14:42:35 +0100

    refactoring

--- src/js/api.js
+++ src/js/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://your-energy.b.goit.study/api';
 
 class Api {
-  #getSearchParams = filter => {
+  #getSearchParams = (filter = 'Muscles') => {
     return {
       filter: filter,
       page: 1,
@@ -11,9 +11,9 @@ class Api {
     };
   };
 
-  async getDataByFilter() {
+  async getDataByFilter(typeFilter) {
     const res = await axios.get('/filters', {
-      params: this.#getSearchParams('Muscles'),
+      params: this.#getSearchParams(typeFilter),
     });
     return res.data;
   }
--- src/js/exercises.js
+++ src/js/exercises.js
@@ -2,10 +2,11 @@ import { data_api } from './api';
 
 const refs = {
   listEx: document.querySelector('.exercises-list'),
+  btnBox: document.querySelector('.exercises-thumb-btn'),
 };
 
-const createImgCard = ({ filter, name, imgURL }) => {
-  return ` <li class='exercises-item'>
+const templateExCard = ({ filter, name, imgURL }) => {
+  return `<li class='exercises-item'>
             <div class='exercises-background-img' style='background-image: url(${imgURL});'>
               <div class="exercises-wrap-info">
                 <p>${name}</p>
@@ -16,8 +17,22 @@ const createImgCard = ({ filter, name, imgURL }) => {
 `;
 };
 
+const renderHtml = data => {
+  const list = data.results.map(i => templateExCard(i)).join('');
+  refs.listEx.innerHTML = list;
+};
+
 try {
   const res = await data_api.getDataByFilter();
-  const list = res.results.map(i => createImgCard(i)).join('');
-  refs.listEx.innerHTML = list;
+  renderHtml(res);
 } catch (error) {}
+
+const onClickBtn = async e => {
+  try {
+    const selectedType = e.target.dataset.type;
+    const res = await data_api.getDataByFilter(selectedType);
+    renderHtml(res);
+  } catch (error) {}
+};
+
+refs.btnBox.addEventListener('click', onClickBtn);
