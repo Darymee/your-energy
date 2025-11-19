e07e3874e61c3c9bd55558a8c9c289e5462d900f
AlexDvor <alexmatvichuk@gmail.com>
2025-11-19 19:27:23 +0100

    add capitalizeStr

--- src/js/template.js
+++ src/js/template.js
@@ -1,9 +1,18 @@
 export const Template = {
   exCard({ filter, name, imgURL }) {
+    const capitalizeStr = str => {
+      if (!str) return;
+      const parseStr = str
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join(' ');
+      return parseStr;
+    };
+
     return `<li class='exercises-item'>
             <div class='exercises-background-img' style='background-image: url(${imgURL});'>
               <div class="exercises-wrap-info">
-                <p>${name}</p>
+                <p>${capitalizeStr(name)}</p>
                 <p>${filter}</p>
               </div>
             </div>
