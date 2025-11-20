06c95093c909089040fbe97424fc58e00652d453
chaikivskti.t <chaikivskyi.t@gmail.com>
2025-11-21 00:08:41 +0100

    fix exersices and add rating modals

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import './exercises';
 import './burger.js';
 import './router.js';
-import './modal-exercises'
+import './modal-exercises';
+import './modal-rating';
 import './footer-subscribe.js';
 import './anchor-button.js';
