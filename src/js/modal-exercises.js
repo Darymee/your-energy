8324455605510c6179c0180156cfda2aeb81e184
Juliya Lipatova <575@ukr.net>
2025-11-21 20:05:51 +0100

    update after merge

--- src/js/modal-exercises.js
+++ src/js/modal-exercises.js
@@ -1,64 +1,74 @@
-import { Modal } from './modal';
+import { Modal, initModalSystem } from './modal';
+import axios from 'axios';
 
-Modal(
-  'exercise',
-  modalExerciseTemplate({
-    _id: '64f389465ae26083f39b17a5',
-    bodyPart: 'waist',
-    equipment: 'body weight',
-    gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0006.gif',
-    name: 'alternate heel touchers',
-    target: 'abs',
-    description:
-      "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They're essential for maintaining posture, stability, and generating force in many movements. Exercises that target the abs include crunches, leg raises, and planks.",
-    rating: 3,
-    burnedCalories: 116,
-    time: 3,
-    popularity: 1,
-  })
-);
+const BASE_URL = 'https://your-energy.b.goit.study/api';
+const overlay = document.querySelector('[data-overlay]');
+const overlayContent = document.querySelector('[data-overlay-content]');
+
+onOpenModal();
+initModalSystem();
+
+async function onOpenModal(e) {
+    try {
+        const { data } = await axios.get(
+            `${BASE_URL}/exercises/64f389465ae26083f39b17a4`
+        );
+
+        const html = modalExerciseTemplate(data);
+        console.log('data', data);
+
+        overlayContent.innerHTML = html;
+        overlay.classList.add('is-open');
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 function makeStars(rating) {
-  const safeRating = Math.max(0, Math.min(5, Number(rating) || 0));
-  const full = Array(safeRating)
-    .fill(
-      `<svg class="full" width="18" height="18">
+    const safeRating = Math.max(0, Math.min(5, rating));
+    const fullCount = Math.floor(safeRating);
+    const emptyCount = 5 - fullCount;
+    const full = Array(fullCount)
+        .fill(
+            `<svg class="full" width="18" height="18">
         <use href="./img/sprite.svg#icon-star"></use>
         </svg>`
-    )
-    .join('');
-  const empty = Array(5 - safeRating)
-    .fill(
-      `<svg class="empty" width="18" height="18">
+        )
+        .join('');
+    const empty = Array(emptyCount)
+        .fill(
+            `<svg class="empty" width="18" height="18">
         <use href="./img/sprite.svg#icon-star"></use>
         </svg>`
-    )
-    .join('');
-  return full + empty;
+        )
+        .join('');
+    return full + empty;
 }
 
 function modalExerciseTemplate(data) {
-  const {
-    name,
-    rating,
-    gifUrl,
-    target,
-    bodyPart,
-    equipment,
-    popularity,
-    burnedCalories,
-    description,
-    time,
-  } = data;
-  const starsHtml = makeStars(rating);
-  return `
+    const {
+        name,
+        rating,
+        gifUrl,
+        target,
+        bodyPart,
+        equipment,
+        popularity,
+        burnedCalories,
+        description,
+        time,
+    } = data;
+    const starsHtml = makeStars(rating);
+    return `
         <div class="modal-exercises">
-            <div class="modal-img-wrapper">
+                <div class="modal-img-wrapper">
                 <img class="modal-img" src="${gifUrl}" alt="${name}" />
             </div>
             <div class="modal-details">
                 <p class="modal-title">${name}</p>
                 <div class="modal-rating">
+
+
                     <div class="modal-rating-value">${rating}</div>
                     <div class="modal-rating-stars">${starsHtml}</div>
                 </div>
@@ -89,13 +99,16 @@ function modalExerciseTemplate(data) {
                 </div>
 
                 <p class="modal-description">${description}</p>
-
-                    <button type="button" class="modal-btn">Add to favorites
+<div class="section-btn">
+                    <button type="button" class="modal-btn favorite-btn">Add to favorites
                     <svg class="modal-btn-icon" width="18" height="18">
                     <use href="./img/sprite.svg#icon-heart"></use>
                     </svg>
                     </button>
-
+                     <button type="button" class="modal-btn rating-btn">Give a rating
+                   
+                    </button>
+</div>
             </div>
         </div>`;
 }
