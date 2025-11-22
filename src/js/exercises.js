fdcf8191c8ee7a95cc370a4deff11ebbf5190d87
AlexDvor <alexmatvichuk@gmail.com>
2025-11-22 13:59:23 +0100

    ref

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -352,6 +352,7 @@ const onClickPaginationBox = async e => {
 /* ---------------- Initialize ---------------- */
 
 getFilteredData();
+console.log('ffff');
 
 /* ---------------- Listeners ---------------- */
 
